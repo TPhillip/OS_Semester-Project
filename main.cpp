468249192f7d0f72f4ed9abeae6650264d5d322e
Tevin Phillip <alias.tdp@gmail.com>
2016-10-03 15:49:29 -0400

    added loader

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//#include <tables.h>
+//#include <loader.h>
+//#include <scheduler.h>
+//#include <dispatcher.h>
+//#include <cpu.h>
+
+using namespace std;
+
+int main(){
+	//loader(); //loads all off given file into simulated disk
+	while(true){
+		//scheduler();  //  take processes from disk and insert fully into simulated . . .	.                        ram, as well as remove finished processes
+		//dispatcher(); //fetches values from ram, converts to binary, sends to cpu 
+		//CPU(); //executes
+		//waitforinterrupt();   //waits for cpu to be done
+	}
+	return 0;
+}
